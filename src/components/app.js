8d2e47f473aabe457a95a1057cb9e402b6034d3b
Ivan Milyutkin <ivan.milyutkin@isolutions.ch>
2016-12-15 13:02:56 +0300

    added terms header component
    added terms template (data protocol)

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import TableHeader from './tableHeader';
+
+const tasks = [
+    {
+        id: 1,
+        task: "task1"
+    },
+    {
+        id: 2,
+        task: "task2"
+    }
+];
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Start</h1>
+                <table>
+                    <TableHeader />
+                </table>
             </div>
         );
     }
 }
+
---
+++ src/components/tableHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class TableHeader extends React.Component {
+    render() {
+        return (
+            <thead>
+                <th>Id</th>
+                <th>Task</th>
+            </thead>
+        );
+    }
+}
+
