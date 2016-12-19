d1b8d70e22b86c0e7444179c164689ce8f5c7d5f
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2016-12-19 22:36:16 +0300

    finished base form for settings with saving (only for name)

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -3,32 +3,50 @@ import Storage from '../../services/storage';
 
 export default class Settings extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        let baseSettings = Storage.get("BaseSettings");
+        if(baseSettings === undefined)
+            baseSettings = { ProjectName: "" };
+
+        this.state = { ProjectName: baseSettings.ProjectName };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ ProjectName: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        Storage.save("BaseSettings", this.state);
+    }
+
     save() {
         Storage.save("BaseSettings", "Lonza");
     }
 
     render() {
-        let baseSettings = Storage.get("BaseSettings");
-        if(baseSettings === undefined)
-            baseSettings = { ProjectName: "" };
-
         return (
             <div className="panel panel-default">
                 <div className="panel-heading">
                     <h1 className="form-header">Project settings.</h1>
                 </div>
                 <div className="panel-body">
-                     <div className="form-horizontal">       
+                     <form className="form-horizontal" onSubmit={ this.handleSubmit }>       
                         <div className="form-group">
                             <label htmlFor="inputEmail3" className="col-sm-3 control-label">Project name</label>
                             <div className="col-sm-5">
-                            <input type="text" className="form-control" id="projectName" placeholder="Project name" value={ baseSettings.ProjectName } />
+                            <input type="text" className="form-control" placeholder="Project name" value={ this.state.ProjectName } onChange={ this.handleChange } />
                             </div>
                         </div>                          
                         <div className="form-group">
                             <label htmlFor="inputEmail3" className="col-sm-3 control-label">Supported languages</label>
                             <div className="col-sm-5">
-                            <input type="text" className="form-control" id="languages" placeholder="Supported languages" />
+                            <input type="text" className="form-control" placeholder="Supported languages" />
                             </div>
                         </div>
                         <div className="form-group">
@@ -36,7 +54,7 @@ export default class Settings extends React.Component {
                             <button type="submit" className="btn btn-default">Save</button>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
            
