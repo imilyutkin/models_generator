a2e91cb82c4df74d67fa2a0587424074a243aa1f
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2017-03-21 22:47:46 +0300

    added some test

--- __tests__/textInputControl.tests.jsx
+++ __tests__/textInputControl.tests.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import TextInputControl from '../src/components/shared/forms/textInputControl.js';
 
 describe('TextInputControl', () => {
+  let onChange = jest.fn();
+  let textInput;
+
+  beforeEach(() => {
+    onChange = jest.fn();
+    textInput = mount(
+      <TextInputControl onChange={onChange} 
+                        initValue="test"
+                        type="text" 
+                        name="Test Field" />
+      );
+  });
+
   it('TextInputControl renders input', () => {
-    const textInput = shallow(<TextInputControl />);
     expect(textInput.find('input')).to.have.length(1);
+  });
+  
+  it('TextInputControl input default value', () => {
+    expect(textInput.find('input').node.value).to.equal("test");
+  });
+
+  it('TextInputControl input check value changing', () => {
+    textInput.simulate('change', { target: { value: 'Name 4' } });
+    expect(textInput.find('input').node.value).to.equal("Name 4");
+  });
+
+  it('TextInputControl input check that on blur work', () => {
+    textInput.simulate('change', { target: { value: 'Name 4' } });
+    textInput.simulate('blur');
   });
 });
